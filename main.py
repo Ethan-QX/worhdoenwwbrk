dc9d02e36cf0d31c7cfefda46b7123854b2e20c7
Ethan-QX <103305008+Ethan-QX@users.noreply.github.com>
2024-10-31 22:30:21 +0800

    jobs

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 # from helper_functions import llm
 from logics.customer_query_handler import process_user_message
 
+from helper_functions import ai_agents
+
 from helper_functions.utility import check_password  
 
 # Check if the password is correct.  
@@ -45,3 +47,27 @@ if form.form_submit_button("Submit"):
 resume = form.text_area("Enter resume link", height=5)
 
 job = form.text_area("Enter Job link", height=5)
+
+
+crew = Crew(
+    agents=[profiler,analyst, resume_strategist],
+    tasks=[extract_requirements, compile_profile,align_with_requirement],
+    verbose=True
+)
+
+
+
+job_application_inputs = {
+    'job_posting_url': job
+    
+    # 'https://www.mycareersfuture.gov.sg/job/information-technology/data-scientist-cpo-03dba75ab1fec49a3aac63d2c676949a?source=MCF&event=Search'
+    # 'https://www.mycareersfuture.gov.sg/job/information-technology/full-stack-developer-ntt-data-singapore-12059bf21549d1794e3535de365d0a77'
+    ,'cv_content':resume
+}
+
+
+### this execution will take a few minutes to run
+
+result = crew.kickoff(inputs=job_application_inputs)
+
+result
